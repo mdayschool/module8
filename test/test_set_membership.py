2631fae87eff41f27565057384c35595ebb5c27b
Michael Day <day.michael.b@gmail.com>
2019-07-07 12:03:43 -0500

    set test red

--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -5,3 +5,6 @@ class SetMembershipTestCase(unittest.TestCase):
     def test_in_set_true(self):
         test_set = {2, 4, 5}
         self.assertEqual(True, set_membership.in_set(test_set, 5))
+    def test_in_set_false(self):
+        test_set = {2, 4, 5}
+        self.assertEqual(False, set_membership.in_set(test_set, 6))
