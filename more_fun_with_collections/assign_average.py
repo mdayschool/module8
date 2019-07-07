d5b28c68661bbe4b6348b8a8885df68514a03fb5
Michael Day <day.michael.b@gmail.com>
2019-07-07 14:51:22 -0500

    switch average green

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,3 +1,7 @@
 
 def switch_average(grade):
-    pass
+    grades = {'A':90, 'B':80, 'C':70, 'D':60, 'F':50}
+    if grade in grades:
+        return grades[grade]
+    else:
+        raise ValueError(grade + " is not a recognized grade.")
